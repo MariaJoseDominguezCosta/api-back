4c7b192a23242e10c74913533b86aa81108d55e2
mahorramas <mahorramas.2020@gmail.com>
2026-07-25 11:16:08 -0600

    Update middlewares.ts
    middleweres

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,8 +2,21 @@ import type { Core } from '@strapi/strapi';
 
 const config: Core.Config.Middlewares = [
   'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
+    'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https://*.tu-dominio-coolify.com'], // Añade el dominio de tu Strapi
+          'media-src': ["'self'", 'data:', 'blob:', 'https://*.tu-dominio-coolify.com'], // Añade el dominio de tu Strapi
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
